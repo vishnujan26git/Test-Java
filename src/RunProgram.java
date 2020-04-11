4b46b277e3a284e58ef914c630650710894473fc
Vishnu <Vishnuswamy5391@gmail.com>
2020-04-11 15:27:25 +0530

    added syntax error

--- src/RunProgram.java
+++ src/RunProgram.java
@@ -4,10 +4,10 @@ public class RunProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
-		System.out.println("Welcome to programming");
-		System.out.println("Welcome to Devops");
-		System.out.println("Welcome to new world");
+		system.out.println("Hello World")
+		//System.out.println("Welcome to programming");
+		//System.out.println("Welcome to Devops");
+		//System.out.println("Welcome to new world");
 	}
 
 }
