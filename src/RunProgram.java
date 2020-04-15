e5714ffe88c41716d921d92853930c1fded2bca5
Vishnu <Vishnuswamy5391@gmail.com>
2020-04-15 22:57:37 +0530

    updated the src file

--- src/RunProgram.java
+++ src/RunProgram.java
@@ -4,7 +4,7 @@ public class RunProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
+		system.out.println("Hello World")
 		//System.out.println("Welcome to programming");
 		//System.out.println("Welcome to Devops");
 		//System.out.println("Welcome to new world");
